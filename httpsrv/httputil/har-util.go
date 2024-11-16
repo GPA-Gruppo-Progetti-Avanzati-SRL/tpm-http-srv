3f702281f4e3e43b7150ba226893edbf3ab0c8ab
M.A.I <mario.imperato@gpagroup.it>
2024-11-16 16:02:44 +0100

    http-util

--- httpsrv/httputil/har-util.go
+++ httpsrv/httputil/har-util.go
@@ -1,10 +1,45 @@
 package httputil
 
 import (
+	"bytes"
 	"github.com/GPA-Gruppo-Progetti-Avanzati-SRL/tpm-http-archive/har"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"io"
 	"strings"
 )
+
+func newHarRequest(c *gin.Context) (*har.Request, error) {
+	const semLogContext = "http-server::har-request-from-http-request"
+
+	var err error
+	var bodyContent []byte
+	if c.Request.ContentLength > 0 {
+		bodyContent, err = io.ReadAll(c.Request.Body)
+		log.Trace().Msgf(semLogContext+" - Lunghezza body :  %d, - Body : %s ,", len(bodyContent), string(bodyContent))
+		if err != nil {
+			log.Error().Err(err).Msg(semLogContext)
+			return nil, err
+		}
+
+		// Put back for subsequent validation phase.
+		c.Request.Body = io.NopCloser(bytes.NewReader(bodyContent))
+	}
+
+	req, err := har.NewRequest(
+		c.Request.Method,
+		c.Request.URL.String(),
+		bodyContent,
+		c.Request.Header,
+		HarQueryStringParamsFromGinParams(c),
+		HarParamsFromGinParams(c))
+	if err != nil {
+		log.Error().Err(err).Msg(semLogContext)
+		return nil, err
+	}
+
+	return req, nil
+}
 
 // HarParamsFromGinParams introduced because of sonar cognitive complexity.
 func HarParamsFromGinParams(c *gin.Context) []har.Param {
